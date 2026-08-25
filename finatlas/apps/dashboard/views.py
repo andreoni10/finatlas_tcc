4b4b8e51e8cf61e5ed0b47994bb1a2ab59306f5b
Lucas Hernandes Andreoni <lucasandreoni2000@gmail.com>
2026-08-25 10:10:48 -0300

    Implementeção de importação e leitura de excel PJ1. Restrição de acesso por cargos.

--- finatlas/apps/dashboard/views.py
+++ finatlas/apps/dashboard/views.py
@@ -1,12 +1,15 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
+from .decorators import cargo_requerido
 
 
-@login_required(login_url="login")
+@cargo_requerido('assessor')
+@login_required
 def advisor_dashboard(request):
     return render(request, "dashboard/dashboard_assessor.html")
 
 
-@login_required(login_url="login")
+@cargo_requerido('financeiro')
+@login_required
 def finance_dashboard(request):
     return render(request, "dashboard/dashboard_financeiro.html")
